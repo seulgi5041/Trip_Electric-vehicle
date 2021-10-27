81a3ece89ab19131827c84e0c035ec4df16af672
seulgi5041 <crudel44@gmail.com>
2021-10-27 15:42:01 +0900

    1027

--- js/responsive.js
+++ js/responsive.js
@@ -11,4 +11,26 @@ $(function(){
 
 
 
+/* 모바일 햄버거 메뉴 클릭 시 나오게 하기 */
+$(".trigger").on({
+    "click" : function(){
+
+        // 햄버거 메뉴 버튼이 보일때(햄버거 메뉴가 -교재215p-visible상태인가?로 판단)
+        if($(".mobile.hamburger img").is(":visible")) {
+            /* 모바일 닫기 버튼 show */
+            $(".mobile.hamburger img").hide();
+            $(".mobile.close img").show();
+            // $("#main_header .nav > li").css("display", "block");
+            
+        } else {
+            // 닫기 메뉴 버튼이 보일때
+            /* 모바일 햄버거 버튼 show */ 
+            $(".mobile.hamburger img").show();
+            $(".mobile.close img").hide();
+        }
+
+    }
+
+});
+
 
