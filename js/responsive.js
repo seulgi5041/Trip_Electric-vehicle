062a3d1d403e38ddad3f74653d201d6e34673229
seulgi5041 <crudel44@gmail.com>
2021-10-31 18:28:49 +0900

    1031

--- js/responsive.js
+++ js/responsive.js
@@ -26,6 +26,7 @@ $(function(){
             // 실질적으로 보이는 부분. 위에 두개속성은 애니메이션 속성을 주기위한것임.
             // .show : 해당 태그의 display를 none에서 block으로 해주는 속성.
             $(".lnb_container").show();
+            $(".lnb_container").css("z-index", "2");
 
             // eq: gnbIndex = $(this).index();태그를 통해서 gnb-li 각각 인덱스와 동일한 lnb div그룹의 배열 번호를 가져와라.
             $(".lnb > ul li").eq(gnbIndex).css("display","block"); //인라인 태그의 우선순위를 이용 
@@ -80,8 +81,9 @@ $(function(){
 
                 var nav = $(".nav").clone();
                 //복제한 nav를 넣어서 id값이 모바일메뉴인 아이에 덧붙여준다.
-                $("#mobile_menu").append(nav); 
+                $("#mobile_menu").append(nav);
                 $("#mobile_menu").show();
+                
                 
             } else {
                 // 닫기 메뉴 버튼이 보일때
