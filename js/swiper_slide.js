eda086d6d1c62d4a0e212485ac6f2c6e2b6a4d95
seulgi5041 <crudel44@gmail.com>
2021-11-03 16:10:37 +0900

    1103

--- js/swiper_slide.js
+++ js/swiper_slide.js
@@ -32,10 +32,33 @@ var swiper2 = new Swiper('.swiper2', {
 // 스와이퍼 1 클릭했을 때 이벤트
 swiper1.on('click', function () {
     // console.log(swiper1.clickedIndex);
-
-    var index = $( ".card-location" ).index( this );
+    var index = swiper1.clickedIndex;
     modalOpen(index);
-
-    console.log(modalOpen);
-
+    // console.log(index);
 });
+
+// 모달 닫기 버튼 클릭 했을 때 이벤트
+$( ".modal_close" ).click(function() {
+    var index = $( ".modal_close" ).index( this );
+    modalClose(index);
+});
+
+//영역 바깥 클릭했을때 닫음
+window.onclick = function(event) {
+    if (event.target.className == "modal") {
+        event.target.style.display = "none";
+    }
+};
+
+
+// 모달 열기
+function modalOpen(idx) {
+    var modals = document.getElementsByClassName("modal");
+    modals[idx].style.display = "block";
+}
+
+//모달 닫기
+function modalClose(idx) {
+    var modals = document.getElementsByClassName("modal");
+    modals[idx].style.display = "none";
+}
