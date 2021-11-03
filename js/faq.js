7f1525f5ac3e2d063cb3c6352c7a6353e9c6e1e2
seulgi5041 <crudel44@gmail.com>
2021-11-03 22:42:53 +0900

    1103

--- js/faq.js
+++ js/faq.js
@@ -37,14 +37,59 @@ $('.hvr-icon').click(function(){
 
         clickFlagBig--;
 
-        console.log(width);     
+        // console.log(width); 
+
     } else if (width >= 570) {
+
+        if (clickFlagMid == 3) {
+            $(".hidden_box").eq(0).show();
+            $(".hidden_box").eq(1).show();
+        } 
+
+
+        if (clickFlagMid == 2) {
+            $(".hidden_box").eq(2).show();
+            $(".hidden_box").eq(3).show();
+        }
+
+        if (clickFlagMid == 1) {
+            $(".hidden_box").eq(4).show();
+            $(".hidden_box").eq(5).show();
+        }
+
         //3번을 클릭해야
         clickFlagMid--;
-        console.log(width);
+        // console.log(width);
+
+        
     } else {
+
+        if (clickFlagSmall == 6) {
+            $(".hidden_box").eq(0).show();
+        }
+
+        if (clickFlagSmall == 5) {
+            $(".hidden_box").eq(1).show();
+        }
+
+        if (clickFlagSmall == 4) {
+            $(".hidden_box").eq(2).show();
+        }
+
+        if (clickFlagSmall == 3) {
+            $(".hidden_box").eq(3).show();
+        }
+
+        if (clickFlagSmall == 2) {
+            $(".hidden_box").eq(4).show();
+        }
+
+        if (clickFlagSmall == 1) {
+            $(".hidden_box").eq(5).show();
+        }
+
         //6번을 클릭해야 나올수 있는
         clickFlagSmall--;
-        console.log(width);
+        // console.log(width);
     }
 });
